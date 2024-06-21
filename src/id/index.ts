15976e5adf429337131c5b8f2364bbf4f5f6b1e2
dan <d@zurly.me>
2024-06-21 22:42:21 +0100

    feat: index + id

---
+++ src/id/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Generates an ID
+ * @param length The length of the ID to generate
+ * @param alphabet The alphabet to choose characters from. Does not dedupe multiples!
+ */
+export function id(
+  length = 20,
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+): string {
+  return [...new Array<never>(length)].reduce(
+    (str) => str + alphabet.charAt(Math.floor(Math.random() * alphabet.length)),
+    ""
+  );
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+throw new Error(
+  "Do not import `d-n` directly, use the submodules (like d-n/prettier)"
+);
+
+export {};
